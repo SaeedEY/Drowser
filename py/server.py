7d6b583927d19b78f4b3712871195c5ba7a01140
Saeed EY <SaeedEY@users.noreply.github.com>
2017-06-07 22:52:03 +0430

    Update server.py

--- py/server.py
+++ py/server.py
@@ -7,45 +7,7 @@ import gui as GUI
 if not GUI.ON:
     print 'Server Offline'
     sys.exit(2)
-"""
-HOST = '127.0.0.1'   
-PORT = GUI.PORT
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# print 'Socket created'
-
-try:
-    s.bind((HOST, PORT))
-except socket.error , msg:
-    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-    if int(msg[0]) == 10048:
-        GUI.alert('Port has Changed To '+str(GUI.PORT+1),'Port Change')
-        GUI.cport('save',PORT+1)
-        GUI.alert('new port is '+str(GUI.PORT),'new Port Change')
-
-        sys.exit(1) and os.system('server.py')
-        
-    else:
-        sys.exit()
-
-# print 'Socket bind complete'
-
-s.listen(10)
-# print 'Socket now listening'
-
-def clientthread(conn):
-    #Sending message to connected client
-    # conn.send('Welcome to the server. Receving Data...\n') 
-
-    #infinite loop so that function do not terminate and thread do not end.
-    while True:
-        #Receiving from client
-        try:
-            data = conn.recv(1024).decode('base64')
-        except Exception, e:
-            print 'Connection Refused'
-        # reply = 'Done!\n'
-"""
 while True:
     file = open('f','a+')
     data = file.read()
@@ -55,23 +17,3 @@ while True:
         GUI.loging("[SERVER_DATA]: ("+str(data+")"))
         f = open('f','w+')
         f.close()
-"""
-        # os.system('server.py')
-        # sys.exit(0)
-        if not data:
-            break
-        # conn.sendall(reply)
-    conn.close()
-
-#now keep talking with the client
-while 1:
-    #wait to accept a connection
-    conn, addr = s.accept()
-    GUI.loging('---------------------------------------------------------')
-    # print 'Connected with ' + addr[0] + ':' + str(addr[1])
-    GUI.loging('[CONNECTED_FROM]:' + addr[0] + ':' + str(addr[1]))
-    #start new thread
-    start_new_thread(clientthread ,(conn,))
-
-s.close()
-"""
